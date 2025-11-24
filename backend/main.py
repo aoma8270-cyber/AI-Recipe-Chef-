adaefe5d7409d1fd95dfedb02993ac74ad4fd380
Aoma Ikegaki <aoma8270@gmail.com>
2025-11-24 19:53:53 +0900

    Fix CORS: all origins

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # 1. CORS設定（Next.jsからのアクセスを許可）
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
